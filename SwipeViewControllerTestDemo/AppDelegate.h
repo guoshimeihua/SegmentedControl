54f43e918e662ddf9819dfee2ce2dd6dba9da83b
BruceGUO <cqdeyouxiang@163.com>
2015-12-22 21:58:47 +0800

    Initial Commit

---
+++ SwipeViewControllerTestDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SwipeViewControllerTestDemo
+//
+//  Created by Bruce on 15/12/22.
+//  Copyright © 2015年 Bruce. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SwipeViewControllerTestDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SwipeViewControllerTestDemo
+//
+//  Created by Bruce on 15/12/22.
+//  Copyright © 2015年 Bruce. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
